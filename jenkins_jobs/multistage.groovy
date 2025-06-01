b505ada0f05621d3e0fed9a43ecf36ae9e75f826
Deleany <artofexe@gmail.com>
2025-06-01 15:32:14 +0300

    lint

--- jenkins_jobs/multistage.groovy
+++ jenkins_jobs/multistage.groovy
@@ -1,34 +1,30 @@
-def sharedVar = 'sharedVar'
+String sharedVar = 'sharedVar'
 
 pipeline {
-    agent any 
+    agent any
 
-    environment{
+    environment {
         GLOBAL_VAR = 'initial'
-
-
     }
-    stages{
-        stage('get token'){
+    stages {
+        stage('get token') {
             steps {
-                script{
+                script {
                     echo "stage 1 Переменная GLOBAL_VAR до изменения ${env.GLOBAL_VAR}"
                     echo "stage 1 Переменная sharedVar до изменения ${sharedVar}"
                     env.GLOBAL_VAR = 'changed in Stage 1'
-                    sharedVar = "changed Data from Stage 1"
+                    sharedVar = 'changed Data from Stage 1'
                 }
             }
         }
-        stage('use token'){
+        stage('use token') {
             steps {
-                script{
+                script {
                     echo "Значение env.GLOBAL_VAR: ${env.GLOBAL_VAR}"
                     echo "Значение GLOBAL_VAR: ${GLOBAL_VAR}"
                     echo "Значение sharedVar: ${sharedVar}"
                 }
             }
-
         }
-    }           
- 
+    }
 }
