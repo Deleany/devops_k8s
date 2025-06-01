4fe57ffef81a0bf7f4f032fd4658138af08effa1
Deleany <artofexe@gmail.com>
2025-06-01 09:56:01 +0300

    add concept multi stage env

---
+++ jenkins_jobs/multistage.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent { any }
+
+    envirinment{
+        GLOBAL_VAR = ''
+        SOME_VAR = 1
+
+    }
+    stages{
+        stage ('get token'){
+
+            script{
+                env.GLOBAL_VAR == 'Hello from Stage 1'
+            }
+
+        stage('user token'){
+            script{
+                println(env.GLOBAL_VAR)
+                println(env.SOME_VAR)
+            }
+        }
+        }
+        
+    }
+    
+}
