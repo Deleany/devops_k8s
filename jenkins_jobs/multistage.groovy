448ff0c1402b4396825469a367c69a970dc4b586
Deleany <artofexe@gmail.com>
2025-06-01 11:19:28 +0300

    change vars

--- jenkins_jobs/multistage.groovy
+++ jenkins_jobs/multistage.groovy
@@ -13,6 +13,8 @@ pipeline {
         stage('get token'){
             steps {
                 script{
+                    echo "stage 1 Переменная GLOBAL_VAR до изменения ${env.GLOBAL_VAR}"
+                    echo "stage 1 Переменная sharedVar до изменения ${sharedVar}"
                     env.GLOBAL_VAR = 'Hello from Stage 1'
                     sharedVar = "Data from Stage 1"
                 }
@@ -21,9 +23,9 @@ pipeline {
         stage('use token'){
             steps {
                 script{
-                    echo "Значение из Stage 1: ${env.GLOBAL_VAR}"
+                    echo "Значение env.GLOBAL_VAR: ${env.GLOBAL_VAR}"
                     println(env.SOME_VAR)
-                    echo "Значение из Stage 1: ${sharedVar}"
+                    echo "Значение sharedVar: ${sharedVar}"
                 }
             }
 
